9d743b7a8ba8e4a074344713fb8a9691183f963d
David Khierl Gidor <dkrgidor@gmail.com>
2021-01-30 03:58:06 +0400

    fix(AccountPage): :bug: responsive layout.

--- src/pages/Account/index.tsx
+++ src/pages/Account/index.tsx
@@ -1,41 +1,65 @@
-import { Box, Divider, Flex, Heading, VStack } from '@chakra-ui/react';
+import { Box, Divider, Flex, Heading, Stack, useMediaQuery } from '@chakra-ui/react';
 import { Route, Switch } from 'react-router-dom';
 import { faAddressCard, faUser } from '@fortawesome/free-solid-svg-icons';
 
+import Container from 'components/common/Container';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import NavLinkRouter from 'components/common/NavLinkRouter';
 import React from 'react';
+import UpdateUserForm from 'components/forms/UpdateUserForm';
 
 const Account = () => {
+  // Divider doesn't support responsive theme
+  const [isLargerThan768] = useMediaQuery('(min-width: 768px)');
+
   return (
-    <Flex p={6} h='100%' flexDirection='column'>
-      <Heading as='h4' size='md'>
-        Settings
-      </Heading>
-      <Flex mt={4} h='100%'>
-        <VStack w='320px' spacing={3}>
-          <NavLinkRouter to='/settings/account' isFullWidth>
-            <Box as='span' mr='10px'>
-              <FontAwesomeIcon icon={faAddressCard} />
-            </Box>
-            Account
-          </NavLinkRouter>
-          <NavLinkRouter to='/settings/profile' isFullWidth>
-            <Box as='span' mr='10px'>
-              <FontAwesomeIcon icon={faUser} />
-            </Box>
-            Profile
-          </NavLinkRouter>
-        </VStack>
-        <Divider orientation='vertical' mx={6} />
-        <div className='px-4 dark:text-white'>
-          <Switch>
-            <Route path='/settings/account'>Account</Route>
-            <Route path='/settings/profile'>Profile</Route>
-          </Switch>
-        </div>
+    <Container my={8} h='100%'>
+      <Heading>Settings</Heading>
+      <Flex mt={4} h='100%' flexDirection='column'>
+        <Flex h='100%' flexDirection={['column', 'column', 'row']}>
+          <Stack
+            w={['288px', '320px']}
+            spacing={3}
+            flexShrink={0}
+            direction={['row', 'row', 'column']}>
+            <NavLinkRouter to='/settings/account' isFullWidth size='sm'>
+              <Box as='span' mr='10px'>
+                <FontAwesomeIcon icon={faAddressCard} />
+              </Box>
+              Account
+            </NavLinkRouter>
+            <NavLinkRouter to='/settings/profile' isFullWidth size='sm'>
+              <Box as='span' mr='10px'>
+                <FontAwesomeIcon icon={faUser} />
+              </Box>
+              Profile
+            </NavLinkRouter>
+          </Stack>
+          <Divider
+            orientation={isLargerThan768 ? 'vertical' : 'horizontal'}
+            mx={[0, 0, 6]}
+            my={[6, 6, 0]}
+          />
+          <Box w='100%'>
+            <Switch>
+              <Route path='/settings/account'>
+                <Container maxWidth={[null, null, '768px']}>
+                  <Heading as='h4' size='md' mb={4}>
+                    General Details
+                  </Heading>
+                  <UpdateUserForm />
+                  <Divider orientation='horizontal' my={6} />
+                  <Heading as='h4' size='md'>
+                    Email
+                  </Heading>
+                </Container>
+              </Route>
+              <Route path='/settings/profile'>Profile</Route>
+            </Switch>
+          </Box>
+        </Flex>
       </Flex>
-    </Flex>
+    </Container>
   );
 };
 
