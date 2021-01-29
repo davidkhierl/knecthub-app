097e99268de0624a147be0bb066df0d092f94bd2
David Khierl Gidor <dkrgidor@gmail.com>
2021-01-30 03:18:47 +0400

    feat(useUserStore): :sparkles: persist user store.

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -1,13 +1,22 @@
 import create from 'zustand';
+import { persist } from 'zustand/middleware';
 
 type UserStore = {
   user: User | null;
   setUser: (user: User) => void;
 };
 
-const useUserStore = create<UserStore>((set) => ({
-  user: null,
-  setUser: (user) => set(() => ({ user }))
-}));
+const useUserStore = create<UserStore>(
+  persist(
+    (set, _get) => ({
+      user: null,
+      setUser: (user) => set(() => ({ user }))
+    }),
+    {
+      name: 'user',
+      getStorage: () => localStorage
+    }
+  )
+);
 
 export default useUserStore;
