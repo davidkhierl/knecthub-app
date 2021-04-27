37f56f516aa968e8fa8f2d9d9a90675959dd20c1
David Khierl Gidor <dkrgidor@gmail.com>
2021-04-27 12:10:53 +0400

    added nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import '../styles/globals.css';
+import 'nprogress/nprogress.css';
 
 import type { AppProps } from 'next/app';
 import Chakra from '@/chakra-ui/chakra';
+import NProgress from 'nprogress';
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import Router from 'next/router';
 import queryClient from '@/lib/queryClient';
+
+NProgress.configure({ showSpinner: false });
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function App({ Component, pageProps }: AppProps) {
   return (
