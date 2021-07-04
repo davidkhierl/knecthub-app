ce948ea4f5e2bbcdfc482caaab5c5defd177377e
David Khierl Gidor <dkrgidor@gmail.com>
2021-07-04 23:09:20 +0400

    add mutation option parameter

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -1,7 +1,7 @@
 import { AxiosError, AxiosResponse } from 'axios';
 
 import api from '@/services/api';
-import { useMutation } from 'react-query';
+import { useMutation, UseMutationOptions } from 'react-query';
 
 export const getAuthLogout = () => api.get('/auth/signout');
 
@@ -13,10 +13,17 @@ interface PostAuthSignIn {
 export const postAuthSignIn = (request: PostAuthSignIn) =>
   api.post<StandardResponse<AuthSuccessResponse>>('/auth/signin', request);
 
-export function useAuthSignInMutation() {
+export function useAuthSignInMutation(
+  options?: UseMutationOptions<
+    AxiosResponse<StandardResponse<AuthSuccessResponse>>,
+    AxiosError<StandardErrorResponse>,
+    PostAuthSignIn,
+    unknown
+  >
+) {
   return useMutation<
     AxiosResponse<StandardResponse<AuthSuccessResponse>>,
     AxiosError<StandardErrorResponse>,
     PostAuthSignIn
-  >(postAuthSignIn);
+  >(postAuthSignIn, options);
 }
