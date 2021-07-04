15f5927eb4eacd7822d1bf29487bf9297995b123
David Khierl Gidor <dkrgidor@gmail.com>
2021-07-04 23:08:42 +0400

    add type comments

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -3,20 +3,59 @@ import create from 'zustand';
 import hasSignedUser from '@/lib/hasSignedUser';
 
 export type AuthStore = {
+  /**
+   * Sets authentication to failed and error object.
+   */
   authFailed: (error?: StandardErrorResponse) => void;
+  /**
+   * Sets authentication to success and user object.
+   */
   authSuccess: (user: User) => void;
+  /**
+   * Boolean value to check if the user is authenticated.
+   */
   authenticated: boolean;
+  /**
+   * Authentication errors.
+   */
   error?: StandardErrorResponse;
+  /**
+   * Authentication in progress, useful for showing loading indicator.
+   */
   isLoading: boolean;
+  /**
+   * Boolean value to check if the user authentication is being processed on the background.
+   */
   isSilentLoadingUser: boolean;
+  /**
+   * Fetch signed-in user data from the api.
+   */
   loadSignedUser: () => void;
+  /**
+   * Sets authentication `isLoading` state.
+   */
   setIsLoading: (loading: boolean) => void;
+  /**
+   * Sets authentication `isSilentLoadingUser` state.
+   */
   setIsSilentLoadingUser: (loading: boolean) => void;
+  /**
+   * Sets user object.
+   */
   setUser: (user: User | null) => void;
+  /**
+   * Sign-out user and remove all data.
+   */
   signOut: () => void;
+  /**
+   * User object.
+   */
   user: User | null;
 };
 
+/**
+ * User authentication store.
+ */
 const useAuthStore = create<AuthStore>((set) => ({
   authenticated: false,
   error: undefined,
@@ -73,4 +112,5 @@ const useAuthStore = create<AuthStore>((set) => ({
   user: null,
   setUser: (user) => set({ user })
 }));
+
 export default useAuthStore;
